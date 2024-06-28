3931329e6671a52ea82f3137453a42e01357a847
shahzainhaider <sshnaqvi2001@gmail.com>
2024-06-28 19:28:02 +0500

    a

--- index.js
+++ index.js
@@ -3,38 +3,31 @@ const express = require("express");
 const cors = require("cors");
 const connectToMongo = require("./database/db.js");
 
-
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
+// Home route
 app.get('/', (req, res) => {
   res.send('Welcome to the Home Page');
 });
 
+// Define other routes
+require("./routes/auth.routes.js")(app);
+
 (async () => {
   try {
-    connectToMongo()
-    AllRoutes();
+     connectToMongo(); // Ensure the connection is established before starting the server
   } catch (error) {
     console.log("Error:", error);
   }
 })();
 
-// YOU CAN ADD MORE ROUTES THERE
-function AllRoutes() {
-  try {
-    require("./routes/auth.routes.js")(app);
-  } catch (error) {
-    console.log("ERROR", error);
-  }
-}
+// Export the app module
 
+const PORT = process.env.PORT || 8080;
 
-
-app.listen(8080, () => {
-  console.log(`Server is running on port 8080.`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
-
-
 module.exports = app;
